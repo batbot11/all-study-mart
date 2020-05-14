c397e9c9386a94c27fe1a97df953eceff2d791ef
Tarun <pathaktarun1111@gmail.com>
2020-05-14 16:25:05 +0530

    Updates

---
+++ precache-manifest.5af60e07d876ca596a67116f71ac9a14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed5d0ce817cba937749edfa713dd7e5",
+    "url": "/all-study-mart/index.html"
+  },
+  {
+    "revision": "7e7573efeb06f109c9eb",
+    "url": "/all-study-mart/static/css/main.faca4cf8.chunk.css"
+  },
+  {
+    "revision": "19de287e554c0460b2eb",
+    "url": "/all-study-mart/static/js/2.f8b40ac0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/all-study-mart/static/js/2.f8b40ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7573efeb06f109c9eb",
+    "url": "/all-study-mart/static/js/main.3fd3477e.chunk.js"
+  },
+  {
+    "revision": "b5cebf7663003b4c7c3e",
+    "url": "/all-study-mart/static/js/runtime-main.f056c5ec.js"
+  }
+]);
